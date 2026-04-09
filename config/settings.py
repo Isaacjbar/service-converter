884285da8812c29e3c3089de9bf0b450ad013088
Isaac Jimenez <isaac777jb@gmail.com>
2026-04-08 23:10:22 -0600

    fix:  x-encrypted

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,18 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'x-encrypted',
+]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
